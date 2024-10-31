534dbb926f653fa239411d295b2a0a29bf0eb0b8
Cole <colekoryto17@gmail.com>
2024-10-30 20:10:22 -0400

    Document configs

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
+# Credentials to connect to the local postgres database
 LOCAL_DB_USER = "postgres"
 LOCAL_DB_PASS = "localpost17"
 LOCAL_DB_NAME = "trees"
 LOCAL_DB_PORT = "5432" # should be 5432 or 5433
 
 # TODO move and reset all credentials before final publish
+# Default credentials to store in the user table as the default admin user
 ADMIN_USERNAME = "admin"
 ADMIN_EMAIL = ""
 ADMIN_NAME = "admin"
 ADMIN_PASSWORD = "temp"
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+# Configurations for the backend user authentication system
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" # to get a string like this run: openssl rand -hex 32
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_HOURS = 24
 
+# Configuration for the database backup service
 BACKUP_INTERVAL_DAYS = 30
